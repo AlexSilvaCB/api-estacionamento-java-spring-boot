1cfbc09d00b02bbd91aa9a942443f3c8fe18b423
AlexSilvaCB <96430292+AlexSilvaCB@users.noreply.github.com>
2023-11-25 16:16:20 -0300

    Created method save user in service and control class

--- src/main/java/silvacb/alex/com/apiestacionamento/controllers/UserController.java
+++ src/main/java/silvacb/alex/com/apiestacionamento/controllers/UserController.java
@@ -1,9 +1,14 @@
 package silvacb.alex.com.apiestacionamento.controllers;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
+import silvacb.alex.com.apiestacionamento.entity.User;
 import silvacb.alex.com.apiestacionamento.services.UserService;
 
 @RequiredArgsConstructor
@@ -12,5 +17,12 @@ import silvacb.alex.com.apiestacionamento.services.UserService;
 public class UserController {
 	
 	private final UserService userService;
+	
+	@PostMapping
+	public ResponseEntity<User> create(@RequestBody User user){
+		User createUser = userService.save(user);
+		return ResponseEntity.status(HttpStatus.CREATED).body(createUser);
+		
+	}
 
 }
--- src/main/java/silvacb/alex/com/apiestacionamento/services/UserService.java
+++ src/main/java/silvacb/alex/com/apiestacionamento/services/UserService.java
@@ -1,8 +1,10 @@
 package silvacb.alex.com.apiestacionamento.services;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
+import silvacb.alex.com.apiestacionamento.entity.User;
 import silvacb.alex.com.apiestacionamento.repository.UserRepository;
 
 
@@ -11,5 +13,10 @@ import silvacb.alex.com.apiestacionamento.repository.UserRepository;
 public class UserService {
 	
 	private final UserRepository userRepository;
+
+	@Transactional
+	public User save(User user) {	
+		return userRepository.save(user);
+	}
 
 }
